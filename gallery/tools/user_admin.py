7de9afa2c196474efdf626ce0708af0284ebdcda
root <root@ip-172-31-10-177.us-east-2.compute.internal>
2020-06-14 03:22:56 +0000

    first commit

---
+++ gallery/tools/user_admin.py
@@ -0,0 +1,105 @@
+import psycopg2
+  
+db_host = "demo-database-1.craoacuwvwbf.us-east-2.rds.amazonaws.com"
+db_name = "image_gallery"
+db_user = "image_gallery"
+
+password_file = "/home/ec2-user/.image_gallery_config"
+
+connection = None
+
+def get_password():
+    f = open(password_file, "r")
+    result = f.readline()
+    f.close()
+    return result[:-1]
+
+def connect():
+    global connection
+    connection = psycopg2.connect(host=db_host, dbname=db_name, user=db_user, password=get_password())
+
+def execute(query, args=None):
+    global connection
+    cursor = connection.cursor()
+    if not args:
+        cursor.execute(query)
+    else:
+        cursor.execute(query, args)
+    return cursor
+
+def main():
+    connect()
+    while True:
+        promptUser()
+        userInput = input("Enter command> ")
+        if userInput == "1":
+            listUsers()
+        elif userInput == "2":
+            addUser()
+        elif userInput == "3":
+            editUser()
+        elif userInput == "4":
+            deleteUser()
+        elif userInput == "5":
+            print("\nBye.")
+            break
+        else:
+            print("\nInvalid Choice.")
+
+def promptUser():
+    print("\n1) List users\n2) Add user\n3) Edit user\n4) Delete user\n5) Quit")
+
+def listUsers():
+    cursor = connection.cursor()
+    result = execute('select * from users')
+    print("\n{: <25} {: <25} {: <25}".format("username", "password", "full name"))
+    print("-------------------------------------------------------------------------")
+    for row in result:
+        print("{: <25} {: <25} {: <25}".format(*row))
+
+def addUser():
+    cursor = connection.cursor()
+    usernameIn = input("\nUsername> ")
+    passwordIn = input("Password> ")
+    fullNameIn = input("Full Name> ")
+    existsQuery = 'select exists (select 1 from users where username=%s)'
+    cursor.execute(existsQuery, (usernameIn,))
+    userExists = cursor.fetchone()[0]
+    if userExists == True:
+        print("\nError: username with username " + usernameIn + " already exists")
+    else:
+        cursor.execute("""
+            insert into users (username, password, full_name)
+            values (%s, %s, %s);
+            """,
+            (usernameIn, passwordIn, fullNameIn,))
+    connection.commit()
+
+def deleteUser():
+    cursor = connection.cursor()
+    userToDelete = input("\nEnter username to delete> ")
+    deleteValidation = input("\nAre you sure you want to delete " + userToDelete + "? ")
+    if deleteValidation.lower() == "yes":
+        print("\nDeleted.")
+        cursor.execute('delete from users where username=%s', (userToDelete,))
+    connection.commit()
+
+def editUser():
+    cursor = connection.cursor()
+    userToEdit = input("\nEnter username to edit> ")
+    existsQuery = 'select exists (select 1 from users where username=%s)'
+    cursor.execute(existsQuery, (userToEdit,))
+    userExists = cursor.fetchone()[0]
+    if userExists == False:
+        print("\nNo such user.")
+    else:
+        newPassword = input("New password (press enter to keep current)> ")
+        if newPassword != "":
+            cursor.execute('update users set password=%s where username =%s', (newPassword, userToEdit))
+        newFullName = input("New full name (press enter to keep current)> ")
+        if newFullName != "":
+            cursor.execute('update users set full_name=%s where username=%s', (newFullName, userToEdit))
+    connection.commit()
+
+if __name__ == '__main__':
+    main()
