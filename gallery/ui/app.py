9e64272643f020f2936149e3658d914784ef67f6
EC2 Default User <ec2-user@ip-10-0-1-77.us-east-2.compute.internal>
2020-06-20 00:14:48 +0000

    M3 Project

---
+++ gallery/ui/app.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+from db_functions import *
+import secrets
+
+app = Flask(__name__)
+
+@app.route('/admin')
+def admin_list():
+    data = listUsers2()
+    return render_template('admin.html', results = data)
+
+@app.route('/admin/addUser')
+def addUser():
+    return render_template('addUser.html')
+
+@app.route('/admin/userAdded', methods=['POST'])
+def userAdded():
+    a = request.form['username']
+    b = request.form['password']
+    c = request.form['full_name']
+    addUser2(a, b, c)
+    return render_template('userAdded.html')
+
+@app.route('/admin/modifyUser')
+def modify_user():
+    return render_template('modifyUser.html')
+
+@app.route('/admin/deleteUser')
+def delete_user():
+    return render_template('deleteUser.html')
+
+@app.route('/admin/userDeleted', methods=['POST'])
+def userDeleted():
+    x = request.form['submit']
+    deleteUser2()
+    return render_template('userDeleted.html')
--- gallery/ui/db_functions.py
+++ gallery/ui/db_functions.py
@@ -57,12 +57,21 @@ def promptUser():
     print("\n1) List users\n2) Add user\n3) Edit user\n4) Delete user\n5) Quit")
 
 def listUsers():
+    connect()
     cursor = connection.cursor()
     result = execute('select * from users')
     print("\n{: <25} {: <25} {: <25}".format("username", "password", "full name"))
     print("-------------------------------------------------------------------------")
     for row in result:
         print("{: <25} {: <25} {: <25}".format(*row))
+
+def listUsers2():
+    connect()
+    cursor = connection.cursor()
+    cursor.execute('select username as us, full_name as fn from users')
+    listResults = cursor.fetchall()
+    print(listResults)
+    return listResults
 
 def addUser():
     cursor = connection.cursor()
@@ -82,6 +91,19 @@ def addUser():
             (usernameIn, passwordIn, fullNameIn,))
     connection.commit()
 
+def addUser2(usernameIn, passwordIn, fullNameIn):
+    connect()
+    cursor = connection.cursor()
+    usernameIn = usernameIn
+    passwordIn = passwordIn
+    fullNameIn = fullNameIn
+    cursor.execute("""
+        insert into users (username, password, full_name)
+        values (%s, %s, %s);
+        """,
+        (usernameIn, passwordIn, fullNameIn,))
+    connection.commit()
+
 def deleteUser():
     cursor = connection.cursor()
     userToDelete = input("\nEnter username to delete> ")
@@ -89,6 +111,12 @@ def deleteUser():
     if deleteValidation.lower() == "yes":
         print("\nDeleted.")
         cursor.execute('delete from users where username=%s', (userToDelete,))
+    connection.commit()
+
+def deleteUser2(userToDeleteIn):
+    cursor = connection.cursor()
+    userToDelete = userToDeleteIn
+    cursor.execute('delete from users where username=%s', (userToDelete,))
     connection.commit()
 
 def editUser():
